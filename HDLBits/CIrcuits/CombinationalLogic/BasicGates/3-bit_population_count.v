92bc7745be3c08347b0292785bd06f160d9deca1
Shin <iakameiloveyou@gmail.com>
2022-07-22 23:31:03 +0800

    Update 2022-7-22

---
+++ HDLBits/CIrcuits/CombinationalLogic/BasicGates/3-bit_population_count.v
@@ -0,0 +1,14 @@
+module top_module(
+    input [2:0] in,
+    output [1:0] out
+);
+
+    integer i;
+    always @(*)begin
+        out = 0;
+        for (i=0;i<=2;i=i+1) begin
+            out = out+in[i];
+        end
+    end
+
+endmodule
